cd1ec5310f2c47043fe120825c32940e499f7c80
pozdrcup <pozdrcup@gmail.com>
2025-12-04 21:53:56 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -6,3 +6,10 @@ class Client:
         self.contact_info = contact_info
         self.registration_date = registration_date or date.today()
         self.notes = notes
+
+class Order:
+    def __init__(self, client_id, order_date=None, description="", amount=0.0):
+        self.client_id = client_id
+        self.order_date = order_date or date.today()
+        self.description = description
+        self.amount = float(amount)
